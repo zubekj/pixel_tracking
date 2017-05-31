ce4dadb862e07279e12a8826d2ac0d529ed5b079
Julian Zubek <zubekj@gmail.com>
2017-06-01 01:56:24 +0200

    Initial results.

--- pixel_change.py
+++ pixel_change.py
@@ -1,69 +1,74 @@
 import numpy as np
 import pandas as pd
 import cv2
+import scipy.signal
+
+ds = 6  # downsample
+cuttoff_freq = 0.75  # for butterworth filtering
 
 
-def dist(cframe, oframe):
-    return ((cframe.astype(np.int8) - oframe.astype(np.int8)) != 0).sum() / 3
-
-
-def calculate_pixel_change(cap, maskA, maskB):
-
+def calculate_pixel_change(cap, masks):
     ret, frame = cap.read()
-    #oframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    oframe = frame
+    oframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     distances = []
 
     while True:
         ret, frame = cap.read()
-
         if not ret:
             break
 
-        #cframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cframe = frame
-        distances.append(dist(cframe, oframe))
+        cframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        distances.append([(np.abs(cframe.astype(np.int8) -
+                                  oframe.astype(np.int8)) & m).mean()
+                          for m in masks])
         oframe = cframe
 
-    return pd.Series(distances)
+    # source http://azitech.wordpress.com/2011/03/15/designing-a-butterworth-low-pass-filter-with-scipy/
+    distances = np.array(distances)
+    norm_pass = cuttoff_freq/(ds/2)
+    norm_stop = 1.5*norm_pass
+    (N, Wn) = scipy.signal.buttord(wp=norm_pass, ws=norm_stop, gpass=2,
+                                   gstop=30, analog=0)
+    (b, a) = scipy.signal.butter(N, Wn, btype='low', analog=0, output='ba')
+    distances = [scipy.signal.lfilter(b, a, distances[:, i])
+                 for i in range(distances.shape[1])]
+    distances = pd.DataFrame(distances).T
+    return distances
 
 
-drawing = False # true if mouse is pressed
-ix,iy = -1,-1
+drawing = False  # true if mouse is pressed
+ix, iy = -1, -1
 img = None
 
+
 # mouse callback function
-def draw_mask(event,x,y,flags,param):
-    global ix,iy,drawing,mode
+def mouse_event(event, x, y, flags, param):
+    global ix, iy, drawing, mode
 
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
-        ix,iy = x,y
+        ix, iy = x, y
 
     elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
-            cv2.circle(img,(x,y),5,(0,0,255),-1)
+        if drawing:
+            cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
-        cv2.circle(img,(x,y),5,(0,0,255),-1)
+        cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
 
 
-if __name__ == "__main__":
-
-    cap = cv2.VideoCapture('flame.avi')
-
+def draw_mask(cap):
     if not cap.isOpened():
         print("Cannot open video file")
         exit(1)
 
     ret, frame = cap.read()
-    #img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     img = frame
 
     cv2.namedWindow('image')
-    cv2.setMouseCallback('image', draw_mask)
+    cv2.setMouseCallback('image', mouse_event)
 
     while(True):
         cv2.imshow('image', img)
@@ -73,6 +78,19 @@ if __name__ == "__main__":
 
     cv2.destroyAllWindows()
 
-    distances = calculate_pixel_change(cap, None, None)
+
+if __name__ == "__main__":
+
+    cap = cv2.VideoCapture('cut.avi')
+    #cap = cv2.VideoCapture('flame.avi')
+    ret, frame = cap.read()
+
+    maskA = np.zeros(frame.shape[:2], dtype=np.int8)
+    maskB = np.zeros(frame.shape[:2], dtype=np.int8)
+
+    maskA[:, list(range(int(maskA.shape[1]/2)))] = 1
+    maskB[:, list(range(int(maskA.shape[1]/2), maskA.shape[1]))] = 1
+
+    distances = calculate_pixel_change(cap, [maskA, maskB])
     distances.to_csv("out.txt", index=False)
     cap.release()
